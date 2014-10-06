ed3b6e75dc34b4b51f3edb6efaf1786dd241e2b4
mobjohnson <mobjohnson@gmail.com>
2014-10-06 18:28:47 -0400

    Edit activites_controller.rb

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,1 +1,23 @@
-activities_controller.rb
+class ActivitiesController < ApplicationController
+	before_action :authenticate_user!, except: [:show, :index]
+
+	def create
+		@outing = Collection.find(params[:outing_id])
+		@activity = @outing.activities.create(activity_params)
+	  redirect_to outing_path(@outing)
+	end
+
+	def destroy
+		@outing = Collection.find(params[:outing_id])
+		@activity = @outing.activities.find(params[:id])
+		@activity.destroy
+		redirect_to outing_path(@outing)
+	end
+
+	private
+
+	def activity_params
+		params.require(:activity).permit(:name, :category, :event_id, :event_url) 
+	end
+
+end

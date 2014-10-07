aa4b669cc63e0bd62c10e1d08d870eefa013c43b
pam- <pam.assogba@gmail.com>
2014-10-07 15:53:48 -0400

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141007145622) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: true do |t|
     t.string  "name"
     t.integer "category"
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 20141007145622) do
     t.integer "user_id"
   end
 
-  add_index "outings", ["user_id"], name: "index_outings_on_user_id"
+  add_index "outings", ["user_id"], name: "index_outings_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 20141007145622) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end

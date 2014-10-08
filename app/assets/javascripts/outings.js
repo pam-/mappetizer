931b037a5cf053cc4d75e658393a72ce249170e0
pam- <pam.assogba@gmail.com>
2014-10-07 22:58:30 -0400

    done for the day, refactored ajax

--- app/assets/javascripts/outings.js
+++ app/assets/javascripts/outings.js
@@ -3,48 +3,28 @@ var geocoder;
 function ready(){
 
 	L.mapbox.accessToken = 'pk.eyJ1IjoicGFtLSIsImEiOiJNT09NSzgwIn0.AWl1AY_kO1HMnFHwxb9mww';
-
 	geocoder = L.mapbox.geocoder('mapbox.places-city-v1'),
   map = L.mapbox.map('map', 'pam-.jmeb29bh');
-	var userId = $('.row').data('id')
-	var locationConfirm = $('.save_location')
-	var date = $('input[type="date"]').val()
-	locationConfirm.hide()
 
-	$('#outing_name').hide()
-	$('.container input[type="date"]').keypress(function(event){
+	var locationConfirm = $('.save_location');
+	var date = $('input[type="date"]').val();
+	locationConfirm.hide();
+	$('#outing_name').hide();
+
+	$('.container input[type="time"]').keypress(function(event){
 		if(event.which === 13) {
 			event.preventDefault();
 			var userLocation = $('#location').val();
-			var defaultName = userLocation + ' Outing';
 			var startDate = $('#start_date').val();
 			var startTime = $('#start_time').val();
 			var endDate = $('#end_date').val();
 			var endTime = $('#end_time').val();
-			console.log(startTime)
+			var outingName = userLocation + ' Outing';
+			var nameField = $('#outing_name input[type="text"]');
+
+			// console.log(startTime)
 			// console.log(defaultName)
 			mapGen(userLocation)
-			locationConfirm.show()
-			locationConfirm.html('<p>Save ' + userLocation + ' outing.</p>');
-			locationConfirm.on('click', function(event){
-				$.ajax({
-					type: 'POST',
-					url: '/outings',
-					dataType: 'json',
-					data: {
-						outing: {
-							name: defaultName,  
-							date: date,
-							user_id: userId,
-							city: userLocation
-						}
-					},
-					success: function(){
-						sideBar(defaultName);
-					}
-				})				
-			});
-
 			eventUrl = 'https://www.eventbriteapi.com/v3/events/search/?venue.city='+userLocation+'&categories=103&start_date.range_start=' + startDate + 'T' + startTime + '%3A00Z&start_date.range_end=' + endDate + 'T' + endTime + '%3A00Z&token=4AP25GNUCXVYPVTGLP3V'
 			$.ajax({
 				type: 'GET',
@@ -53,28 +33,22 @@ function ready(){
 					console.log(result.events)
 					return render(result.events)
 				}
-			})
-		}
-	})
-}
+			})			
 
-function sideBar(defaultName){
-	$('.save_location').hide();
-	$('#outing_name').show();
-	var nameField = $('#outing_name input[type="text"]');
-	nameField.attr('placeholder', defaultName);	
-	nameField.keypress(function(event){
-		if(event.which === 13){
-			event.preventDefault();
-			var outingName = $(this).val();
-			$.ajax({
-				type: 'PATCH',
-				url: 'outings/' + outingId,
-				data: { outing: { name: outingName } },
-				success: function(){
-					$('#outing_name').html(outingName);
+			$('#outing_name').show();
+			nameField.attr('placeholder', outingName);
+
+			nameField.keypress(function(event){
+				if(event.which === 13){
+					event.preventDefault();
+					outingName = $(this).val();
 				}
 			})
+			locationConfirm.show()
+			locationConfirm.html('<p>Save ' + userLocation + ' outing.</p>');
+			locationConfirm.on('click', function(event){
+				sideBar(userLocation, startDate, startTime, endDate, endTime, outingName);				
+			});
 		}
 	})
 }
@@ -126,5 +100,34 @@ function markerGen(longitude, latitude, venue_name){
 	});
 }
 
+function sideBar(userLocation, startDate, startTime, endDate, endTime, outingName){
+	var userId = $('.row').data('id');
+	$('.save_location').hide();
+	$.ajax({
+		type: 'POST',
+		url: '/outings',
+		dataType: 'json',
+		data: {
+			outing: {
+				name: outingName,  
+				date: startDate,
+				user_id: userId,
+				city: userLocation
+			}
+		},
+		success: function(){
+			$('.new_outing').hide();
+			displayOutingInfo(outingName, startDate, startTime, endDate, endTime, userLocation)
+		}
+	})
+}
+
+function displayOutingInfo(name, start, sTime, end, eTime, location){
+	var infoContainer = $('.new_outing_info');
+	infoContainer.addClass('active');
+
+	$(infoContainer, 'h2').html(name);
+
+}
 
 $(document).ready(ready);
